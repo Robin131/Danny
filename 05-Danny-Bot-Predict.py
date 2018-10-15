e47cab5bee71ccc60cc11b4ed7c72901595721cc
Robin131 <zhangnan2011@sohu.com>
2018-10-15 14:31:13 -0400

    Merge branch 'master' of C:\Users\zhannan\Desktop\Danny with conflicts.

--- 05-Danny-Bot-Predict.py
+++ 05-Danny-Bot-Predict.py
@@ -24,41 +24,29 @@ model = seq2seq_wrapper.Seq2Seq(xseq_len=xseq_len,
                                xvocab_size=xvocab_size,
                                yvocab_size=yvocab_size,
                                ckpt_path='ckpt/danny/',
-                               loss_path='ckpt/danny/preset',
-                               metadata=metadata,
                                emb_dim=emb_dim,
                                num_layers=3
                                )
 
 sess = model.restore_last_session()
 
-input_txt = ''
+text = ['hello', 'how', 'are', 'you']
 
-while not input_txt == '[End]':
-    input_txt = input()
-    question = data.split_sentence(input_txt, metadata)
-    input_ = question.T
-    output_ = model.predict(sess, input_)
-    answer = data_utils.decode(sequence=output_[0], lookup=metadata['idx2w'], separator=' ')
+question = np.array(data_utils.encode(sequence=text, lookup=metadata['w2idx']))
+for i in range(0, 25 - len(text)):
+    question = np.append(question, 0)
 
+question = [question]
+question = np.array(question)
+print(question)
 
-# text = ['hi']
-#
-# question = np.array(data_utils.encode(sequence=text, lookup=metadata['w2idx']))
-# for i in range(0, 25 - len(text)):
-#     question = np.append(question, 0)
-#
-# question = [question]
-# question = np.array(question)
-# print(question)
-#
-# input = question.T
-# print(input)
-# output = model.predict(sess, input)
-# print(output)
-# for i in output:
-#     answer = data_utils.decode(sequence=i, lookup=metadata['idx2w'], separator=' ')
-#     print(answer)
+input = question.T
+print(input)
+output = model.predict(sess, input)
+print(output)
+for i in output:
+    answer = data_utils.decode(sequence=i, lookup=metadata['idx2w'], separator=' ')
+    print(answer)
 
 
 
